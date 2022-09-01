8b074ead2b93695977f50781f3c832afae312466
Никита Гринченко <Grin_wm40@student.itstep.org>
2022-09-01 15:43:15 +0300

    Add project files.

---
+++ WinForms/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinForms
+{
+    public partial class Form1 : Form
+    {
+        private static int count;
+        public Form1()
+        {
+            count = 0;
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            count++;
+            button1.Text = count.ToString();
+        }
+    }
+}
