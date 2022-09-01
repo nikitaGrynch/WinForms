5d11e94c7b957336b5579bc07656b589d8c66ea6
Никита Гринченко <Grin_wm40@student.itstep.org>
2022-09-01 17:44:37 +0300

    Practice 3

--- WinForms/Form1.Designer.cs
+++ WinForms/Form1.Designer.cs
@@ -51,6 +51,9 @@
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
             this.ResumeLayout(false);
 
         }
--- WinForms/Form1.cs
+++ WinForms/Form1.cs
@@ -12,6 +12,10 @@ namespace WinForms
 {
     public partial class Form1 : Form
     {
+        private bool isPressed = false;
+        private Button newButton;
+        private Point prevPos;
+
         private int counter;
         public Form1()
         {
@@ -27,16 +31,79 @@ namespace WinForms
 
         private void button1_MouseEnter(object sender, EventArgs e)
         {
-            Random rnd = new Random();
-            //button1.BackColor = Color.Red;
-            int newX = rnd.Next(this.Size.Width - button1.Size.Width);
-            int newY = rnd.Next(this.Size.Height - button1.Size.Height);
-            button1.Location = new Point(newX, newY);
+            
         }
 
         private void button1_MouseLeave(object sender, EventArgs e)
         {
-            // button1.BackColor = Color.Blue;
+            
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            counter++;
+            isPressed = true;
+            newButton = new Button();
+            var currentMousePos = this.PointToClient(Cursor.Position);
+            prevPos = currentMousePos;
+            newButton.Location = new Point(currentMousePos.X, currentMousePos.Y);
+            newButton.Size = new Size(0, 0);
+            newButton.Text = counter.ToString();
+            this.Controls.Add(newButton);
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            isPressed = false;
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isPressed)
+            {
+                var currentMousePos = this.PointToClient(Cursor.Position);
+                int newSizeX = 0;
+                int newSizeY = 0;
+                int newLocationX = 0;
+                int newLocationY = 0;
+
+                bool isChangeLocation = false;
+                if(currentMousePos.X - prevPos.X > 0 && currentMousePos.Y - prevPos.Y > 0)
+                {
+                    newSizeX = currentMousePos.X - prevPos.X;
+                    newSizeY = currentMousePos.Y - prevPos.Y;
+                }
+                else if(currentMousePos.X - prevPos.X < 0 && currentMousePos.Y - prevPos.Y > 0)
+                {
+                    newLocationX = currentMousePos.X;
+                    newLocationY = prevPos.Y;
+                    newSizeX = prevPos.X - currentMousePos.X;
+                    newSizeY = currentMousePos.Y - prevPos.Y;
+                    isChangeLocation = true;
+                }
+                else if (currentMousePos.X - prevPos.X > 0 && currentMousePos.Y - prevPos.Y < 0)
+                {
+                    newLocationX = prevPos.X;
+                    newLocationY = currentMousePos.Y;
+                    newSizeX = currentMousePos.X - prevPos.X;
+                    newSizeY = prevPos.Y - currentMousePos.Y;
+                    isChangeLocation = true;
+                }
+                else if(currentMousePos.X - prevPos.X < 0 && currentMousePos.Y - prevPos.Y < 0)
+                {
+                    newLocationX = currentMousePos.X;
+                    newLocationY = currentMousePos.Y;
+                    newSizeX = prevPos.X - currentMousePos.X;
+                    newSizeY = prevPos.Y - currentMousePos.Y;
+                    isChangeLocation = true;
+
+                }
+                if (isChangeLocation)
+                    newButton.Location = new Point(newLocationX, newLocationY);
+
+                Size newSize = new Size(newSizeX, newSizeY);
+                newButton.Size = newSize;
+            }
         }
     }
 }
