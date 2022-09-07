7a6c8ccfb2a6082bd87e7f9452ec5bcaa576a5c9
Никита Гринченко <Grin_wm40@student.itstep.org>
2022-09-07 11:51:24 +0300

    EndlessMenu

---
+++ WinForms/EndlessMenuForm.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinForms
+{
+    public partial class EndlessMenuForm : Form
+    {
+        private int counter;
+        private MenuStrip menuStrip;
+        private ToolStripMenuItem prevMenuItem;
+        public EndlessMenuForm()
+        {
+            InitializeComponent();
+            counter = 1;
+            menuStrip = new MenuStrip();
+            menuStrip.Dock = DockStyle.Top;
+            prevMenuItem = new ToolStripMenuItem();
+            prevMenuItem.Text = counter.ToString();
+            menuStrip.Items.Add(prevMenuItem);
+            counter++;
+            menuStrip.MouseHover += CreateItem;
+            this.Controls.Add(menuStrip);
+            
+        }
+
+        private void CreateItem(object sender, EventArgs e)
+        {
+            var menuItem = new ToolStripMenuItem();
+            menuItem.Text = counter.ToString();
+            prevMenuItem.DropDownItems.Add(menuItem);
+            menuItem.MouseHover += CreateItem;
+            counter++;
+            prevMenuItem = menuItem;
+        }
+    }
+}
--- WinForms/Program.cs
+++ WinForms/Program.cs
@@ -16,7 +16,7 @@ namespace WinForms
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new ColorDialogForm());
+            Application.Run(new EndlessMenuForm());
         }
     }
 }
